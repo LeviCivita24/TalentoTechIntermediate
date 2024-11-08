7e02bff59c79b0380be7c299b33ac88b83b9ef7a
LeviCivita24 <choyospelaez@gmail.com>
2024-11-08 16:02:40 -0500

    update

---
+++ SQL2/Consultas.sql
@@ -0,0 +1,8 @@
+use SectorMineroEnergetico;
+select * from tipos_energia; 
+select * from proyectos; 
+select * from minerales; 
+
+# Consulta 1
+# Filtrar proyectos desarrollados en andes para fechas mayores a 2023. 
+
